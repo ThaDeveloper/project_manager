c918bf3a4ec0aa894dbd4d0a6b27f35dbec9c82a
Justin Ndwiga <ndwigajustin@gmail.com>
2018-04-10 23:13:16 +0300

    Get request from a todos API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
+import $ from 'jquery';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
+import Todos from './Components/Todos';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +11,24 @@ class App extends Component {
     super();
     this.state = { projects: []}
   }
+
+  getTodos(){
+    $.ajax({
+      url: 'https://jsonplaceholder.typicode.com/todos',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({todos: data}, function(){
+          console.log(this.state);
+        });
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+      }
+    });
+  }
 //lifecycle method to update state
-  componentWillMount(){
+  getProjects(){
     this.setState({projects: [
       {
         id: uuid.v4(),
@@ -28,6 +46,15 @@ class App extends Component {
         category: 'Web Development'
       }
     ]});
+  }
+
+  componentWillMount () {
+    this.getProjects();
+    this.getTodos();
+  }
+
+  componentDidMount() {
+    this.getTodos();
   }
 
   handleAddProject(project){
@@ -49,6 +76,8 @@ class App extends Component {
         My App
         <AddProject addProject={this.handleAddProject.bind(this)}/>
         <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
+        <hr />
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
